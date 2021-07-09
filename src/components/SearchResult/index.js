ce248b960671665a1367b1b56e94f05ebcdcda64
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-07-09 06:05:35 +0600

    Responsive: Search result accordion added

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -1,5 +1,17 @@
-import { Box, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+
+import {
+  Box,
+  makeStyles,
+  Typography,
+  Hidden,
+  Accordion,
+  AccordionActions,
+  AccordionSummary,
+  AccordionDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 //styles
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,10 +28,18 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     color: theme.palette.text.secondary,
     fontSize: 16,
+    [theme.breakpoints.down("sm")]: {
+      "& .MuiTypography-body1": {
+        fontSize: 15,
+      },
+    },
   },
   city__name: {
     fontSize: 18,
     fontWeight: 500,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 16,
+    },
   },
   filter__icon: {
     marginRight: theme.spacing(1.5),
@@ -28,6 +48,13 @@ const useStyles = makeStyles((theme) => ({
   arrow__icon: {
     marginLeft: theme.spacing(2.5),
   },
+  accordion__root: {
+    boxShadow: "none",
+    borderRadius: 4,
+    "&::before": {
+      opacity: 0,
+    },
+  },
 }));
 
 //functional component
@@ -35,23 +62,58 @@ const SearchResult = () => {
   const classes = useStyles();
 
   return (
-    <Box>
-      <Box className={classes.root}>
-        <Typography className={classes.city__name} component="h3">
-          Calabasas, CA
-        </Typography>
-        <Box className={classes.details__box}>
-          <img className={classes.filter__icon} src="/assets/FilterIcon.svg" />
-          <Typography component="h3">
-            $1.5k+ / 2 Beds / Appt. ... 3 more
-          </Typography>
-          <img
-            className={classes.arrow__icon}
-            src="/assets/RightArrowIcon.svg"
-          />
-        </Box>
+    <>
+      <Box>
+        <Hidden smDown>
+          <Box className={classes.root}>
+            <Typography className={classes.city__name} component="h3">
+              Calabasas, CA
+            </Typography>
+            <Box className={classes.details__box}>
+              <img
+                className={classes.filter__icon}
+                src="/assets/FilterIcon.svg"
+              />
+              <Typography component="h3">
+                $1.5k+ / 2 Beds / Appt. ... 3 more
+              </Typography>
+              <img
+                className={classes.arrow__icon}
+                src="/assets/RightArrowIcon.svg"
+              />
+            </Box>
+          </Box>
+        </Hidden>
       </Box>
-    </Box>
+      <Hidden smUp>
+        <Accordion className={classes.accordion__root}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.city__name} component="h3">
+              Calabasas, CA
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box className={classes.details__box}>
+              <img
+                className={classes.filter__icon}
+                src="/assets/FilterIcon.svg"
+              />
+              <Typography component="h3">
+                $1.5k+ / 2 Beds / Appt. ... 3 more
+              </Typography>
+              <img
+                className={classes.arrow__icon}
+                src="/assets/RightArrowIcon.svg"
+              />
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+      </Hidden>
+    </>
   );
 };
 
